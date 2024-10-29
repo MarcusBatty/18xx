f98de0f8d295f3541cb6bfa26de5de5909a19ef8
Marcus Batty <lewisthornberry@yahoo.com>
2024-10-29 17:08:11 -0400

    issue share price change log

--- lib/engine/game/g_18_il/step/issue_shares.rb
+++ lib/engine/game/g_18_il/step/issue_shares.rb
@@ -8,9 +8,11 @@ module Engine
       module Step
         class IssueShares < Engine::Step::IssueShares
           def process_sell_shares(action)
-            #@log << "process_sell_shares"
             @game.sell_shares_and_change_price(action.bundle)
+            old = action.bundle.corporation.share_price.price
             @game.stock_market.move_left(action.bundle.corporation) 
+            new = action.bundle.corporation.share_price.price
+            @log << "#{action.bundle.corporation.name}'s share price moves left from #{old} to #{new}"
             pass!
           end
         end
