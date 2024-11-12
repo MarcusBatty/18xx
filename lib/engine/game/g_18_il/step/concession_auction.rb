25c777c947afe2dbd722536ea0129fd00f4d5749
Scott Ninmer <scninmer08@gmail.com>
2024-11-12 05:52:03 -0500

    concession auction player order

--- lib/engine/game/g_18_il/step/concession_auction.rb
+++ lib/engine/game/g_18_il/step/concession_auction.rb
@@ -8,6 +8,15 @@ module Engine
       module Step
         class ConcessionAuction < Engine::Step::ConcessionAuction
           
+
+
+          def setup
+            @game.players.each(&:unpass!)
+            setup_auction   
+            #@companies = @game.companies
+            @companies = @game.companies.select { |company| company.meta[:type] == :concession }
+          end
+         
           def description
             if @auctioning
               'Bid on Selected Concession'
@@ -15,12 +24,31 @@ module Engine
               'Bid on Concession'
             end
           end
-
-          def setup
-            @game.players.each(&:unpass!)
-            setup_auction   
-            #@companies = @game.companies
-            @companies = @game.companies.select { |company| company.meta[:type] == :concession }
+          
+          def resolve_bids
+            return unless @bids[@auctioning].one?
+    
+            bid = @bids[@auctioning].first
+            @auctioning = nil
+            price = bid.price
+            company = bid.company
+            player = bid.entity
+            @bids.delete(company)
+            buy_company(player, company, price)
+            @round.next_entity_index!
+          end
+          
+          def buy_company(player, company, price)
+            if (available = max_bid(player, company)) < price
+              raise GameError, "#{player.name} has #{@game.format_currency(available)} available and cannot spend #{@game.format_currency(price)}"
+            end
+            company.owner = player
+            player.companies << company
+            player.spend(price, @game.bank) if price.positive?
+            @companies.delete(company)
+            @log << "#{player.name} wins the auction for #{company.name} with a bid of #{@game.format_currency(price)}"
+            @game.players.insert((@round.entity_index - 1), @game.players.delete_at(@game.players.index(player)))
+            @round.entity_index = @game.players.index(player)
           end
           
         end
